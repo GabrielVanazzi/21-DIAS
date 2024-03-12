8f149a320e81ac3ea0d24c506183c4ee0ac12f64
Gabriel L. Vanazzi <gabrielvanazzi@gmail.com>
2024-03-11 21:50:17 -0300

    criando repositorio no dia 3

--- dia 3/index.js
+++ dia 3/index.js
@@ -1,12 +1,12 @@
 let nome = ""
 let idade = 0
 let peso = 0
-let altura = 0
+let altura = 0 
 
-nome = prompt(' insira seu nome: ') 
-idade = parseInt(prompt(' digite sua idade : '))
-peso = parseInt(prompt(' insira seu peso: '))
-altura = parseFloat(prompt(' digite sua altura: '))
+nome = prompt('digite seu nome: ')
+idade = parseInt(prompt('digite sua idade'))
+peso = parseInt(prompt(' digite seu peso'))
+altura = parseFloat(prompt('digite sua altura'))
 
 let anoNasc = 0
 anoNasc = 2024 - idade
@@ -14,5 +14,5 @@ anoNasc = 2024 - idade
 let imc = 0
 imc = peso / (altura * altura)
 
-console.log( "Olá " + nome + " você têm " + idade + " anos, nasceu em " + anoNasc + ", seu peso é de " + peso + 
-"kg, sua altura é de " + altura + "M e seu IMC é de " + imc + "kg/m2. Obrigado! ") 
+console.log(" Olá " +nome+ " vimos que você têm " +idade+ " anos, seu peso é de " +peso+ "Kg e sua altura é de " 
++altura+ "M, também temos se IMC, que é de " +imc+ "kg/m2. Obrigado!!") 
