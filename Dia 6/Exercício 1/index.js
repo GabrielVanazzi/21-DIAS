8139ac1f3999d986040daf6d179700ad0685cb0a
Gabriel L. Vanazzi <gabrielvanazzi@gmail.com>
2024-03-31 14:33:58 -0300

    Criando repositório no dia 6

---
+++ Dia 6/Exercício 1/index.js
@@ -0,0 +1,17 @@
+//CRIANDO VARIAVEIS
+let valor = parseInt(prompt("Digite um valor inteiro e positivo:"))
+
+// #1 CRIANDO LOOP FOR
+for(let i = 0; i <= valor; i++){
+    console.log(i)
+}
+
+// #2 CRIANDO LOOP FOR
+for(let i = 0; i <= valor; i += 5){
+    console.log(i)
+}
+
+// #3 CRIANDO LOOP FOR
+for(let i = valor; i >= 0; i -= 5){
+    console.log(i)
+}
